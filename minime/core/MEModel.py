239bc6b9abbb343b758d7aa94124774118c9f260
Ali Ebrahim <aebrahim@ucsd.edu>
2015-01-21 03:38:41 -0800

    functions to extract flux states from solutions
    Extract metabolic, transcription and translation fluxes.
    
    Fixes #5

--- minime/core/MEModel.py
+++ minime/core/MEModel.py
@@ -1,5 +1,7 @@
 from cobra import Model, DictList
 
+from minime.core.MEReactions import *
+
 
 class MEmodel(Model):
     def __init__(self, *args):
@@ -8,3 +10,51 @@ class MEmodel(Model):
         self.complex_data = DictList()
         self.translation_data = DictList()
         self.transcription_data = DictList()
+
+    def get_metabolic_flux(self, solution=None):
+        """extract the flux state for metabolic reactions"""
+        if solution is None:
+            solution = self.solution
+            if solution.status != "optimal":
+                raise ValueError("model status '%s' is not 'optimal'" %
+                                 solution.status)
+        flux_dict = {r.id: 0 for r in self.metabolic_reaction_data}
+        for reaction in self.reactions:
+            if isinstance(reaction, MetabolicReaction):
+                m_reaction_id = reaction.metabolic_reaction_data.id
+                if reaction.reverse:
+                    flux_dict[m_reaction_id] -= solution.x_dict[reaction.id]
+                else:
+                    flux_dict[m_reaction_id] += solution.x_dict[reaction.id]
+        return flux_dict
+
+    def get_transcription_flux(self, solution=None):
+        """extract the transcription flux state"""
+        if solution is None:
+            solution = self.solution
+            if solution.status != "optimal":
+                raise ValueError("model status '%s' is not 'optimal'" %
+                                 solution.status)
+        flux_dict = {}
+        for reaction in self.reactions:
+            if isinstance(reaction, TranscriptionReaction):
+                for rna_id in reaction.transcription_data.RNA_products:
+                    locus_id = rna_id.replace("RNA_", "", 1)
+                    if locus_id not in flux_dict:
+                        flux_dict[locus_id] = 0
+                    flux_dict[locus_id] += solution.x_dict[reaction.id]
+        return flux_dict
+
+    def get_translation_flux(self, solution=None):
+        """extract the translation flux state"""
+        if solution is None:
+            solution = self.solution
+            if solution.status != "optimal":
+                raise ValueError("model status '%s' is not 'optimal'" %
+                                 solution.status)
+        flux_dict = {r.id: 0 for r in self.translation_data}
+        for reaction in self.reactions:
+            if isinstance(reaction, TranslationReaction):
+                protein_id = reaction.translation_data.id
+                flux_dict[protein_id] += solution.x_dict[reaction.id]
+        return flux_dict
