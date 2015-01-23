40ea7617acbcdf3428deae1597b6ffc6df687d30
Ali Ebrahim <aebrahim@ucsd.edu>
2015-01-23 14:39:46 -0800

    substitute in symbolic bounds for lp as well

--- minime/solve/symbolic.py
+++ minime/solve/symbolic.py
@@ -5,8 +5,15 @@ from minime import mu
 
 
 def _compile(expr):
-    """compiles a sympy expression, and returns None otherwise"""
-    return lambdify(mu, expr) if isinstance(expr, Basic) else None
+    """compiles a sympy expression"""
+    return lambdify(mu, expr) if isinstance(expr, Basic) else expr
+
+
+def _eval(expr, mu):
+    """evaluate the expression as a function of mu
+
+    will return the expr if it is not a function"""
+    return expr(mu) if callable(expr) else expr
 
 
 def compile_expressions(me_model):
@@ -14,10 +21,9 @@ def compile_expressions(me_model):
 
     The compiled expressions dict has the following key value pairs:
     (met_index, rxn_index): stoichiometry,
-    (None, rxn_index): (lower_bound or None, upper_bound or None)
-    (met_index, None): met_bound
+    (None, rxn_index): (lower_bound, upper_bound)
+    (met_index, None): (met_bound, met_constraint_sense)
 
-    Any non-None value in the dict can be assumed to be a compiled function
     """
     expressions = {}
     for i, r in enumerate(me_model.reactions):
@@ -26,13 +32,16 @@ def compile_expressions(me_model):
             if isinstance(stoic, Basic):
                 expressions[(me_model.metabolites.index(met), i)] = \
                     lambdify(mu, stoic)
-        bounds = (_compile(r.lower_bound), _compile(r.upper_bound))
-        if bounds[0] is not None or bounds[1] is not None:
-            expressions[(None, i)] = bounds
+        # If either the lower or upper reaction bounds are symbolic
+        if isinstance(r.lower_bound, Basic) or \
+                isinstance(r.upper_bound, Basic):
+            expressions[(None, i)] = (_compile(r.lower_bound),
+                                      _compile(r.upper_bound))
+    # Metabolite bound
     for i, metabolite in enumerate(me_model.metabolites):
-        compiled = _compile(metabolite._bound)
-        if compiled is not None:
-            expressions[(i, None)] = compiled
+        if isinstance(metabolite._bound, Basic):
+            expressions[(i, None)] = (_compile(metabolite._bound),
+                                      metabolite._constraint_sense)
     return expressions
 
 
@@ -40,8 +49,9 @@ def substitute_mu(lp, mu, compiled_exressions):
     """substitute mu into a constructed LP"""
     for index, expr in iteritems(compiled_exressions):
         if index[0] is None:  # reaction bounds
-            print "not implemented"
+            lp.change_variable_bounds(index[1],
+                                      _eval(expr[0], mu), _eval(expr[0], mu))
         elif index[1] is None:  # metabolite _bound
-            print "not implemented"
+            lp.change_constraint(index[0], expr[1], _eval(expr[0], mu))
         else:  # stoichiometry
             lp.change_coefficient(index[0], index[1], expr(mu))
