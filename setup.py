162deacafa38d6084c47e8c37533c691e70d82eb
coltonlloyd <cjlloyd@eng.ucsd.edu>
2018-03-24 13:37:18 -0700

    To version 0.0.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(name="cobrame",
-      version="0.0.7",
+      version="0.0.8",
       author="Colton Lloyd and Ali Ebrahim",
       url="https://github.com/SBRG/cobrame",
       install_requires=["sympy", "six", "Biopython", "cobra<=0.5.11", "pandas",
